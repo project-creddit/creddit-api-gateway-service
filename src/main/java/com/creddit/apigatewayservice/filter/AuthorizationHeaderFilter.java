a7a6bb8249f5765206f100b8c49e0bf7ab4ae2a2
oereo <dlstpgns0406@gmail.com>
2022-04-30 20:23:34 +0900

    feat: onError private method 추가

--- src/main/java/com/creddit/apigatewayservice/filter/AuthorizationHeaderFilter.java
+++ src/main/java/com/creddit/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -64,6 +64,13 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         return returnValue;
     }
 
+    // Mono, Flux -> Spring WebFlux
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
 
     public static class Config {
         // Put configuration properties here
